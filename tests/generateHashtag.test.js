1dacbc96e2a814464445f609a94d6740f36497d1
Sleaf <shu.huang@mail.hypers.com>
2019-12-12 18:11:05 +0800

    摸leetcode

--- tests/generateHashtag.test.js
+++
@@ -1,9 +0,0 @@
-const solve = require('../src').generateHashtag;
-
-test('example', () => {
-  expect(solve(" Hello there thanks for trying my Kata")).toBe('#HelloThereThanksForTryingMyKata');
-  expect(solve(" Hello     World   ")).toBe('#HelloWorld');
-  expect(solve("")).toBe(false);
-  expect(solve("Codewars")).toBe("#Codewars");
-  expect(solve("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong")).toBe(false);
-});
--- tests/isInteresting.test.js
+++
@@ -1,9 +0,0 @@
-const solve = require('../src').isInteresting;
-
-test('example', () => {
-  expect(solve(3, [1337, 256])).toBe(0);
-  expect(solve(11209, [1337, 256])).toBe(1);
-  expect(solve(11211, [1337, 256])).toBe(2);
-  expect(solve(9876, [1337, 256])).toBe(2);
-  expect(solve(3210, [1337, 256])).toBe(2);
-});
